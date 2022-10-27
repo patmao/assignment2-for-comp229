3cae67e1062b5a54747cfc7a8d13ec4b19287eb0
Yin-Siangl <ymao6@my.centennialcollege.ca>
2022-10-26 20:44:57 -0400

    Finish everything, connected to Atlas and now going to upload to Heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://patrick:8oiXIFk1Rk2KnJuy@mongodbserver.m9zf7vr.mongodb.net/book_store?retryWrites=true&w=majority"
+
 }
