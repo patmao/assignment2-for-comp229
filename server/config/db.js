9133a8dba1e938312bd54f1d57ea78f05423e1b8
Yin-Siangl <ymao6@my.centennialcollege.ca>
2022-10-27 21:55:29 -0400

    try connect to Heroku

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@
 
 module.exports = 
 {
-    //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://patrick:8oiXIFk1Rk2KnJuy@mongodbserver.m9zf7vr.mongodb.net/book_store?retryWrites=true&w=majority"
+    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb+srv://patrick:8oiXIFk1Rk2KnJuy@mongodbserver.m9zf7vr.mongodb.net/book_store?retryWrites=true&w=majority"
 
 }
